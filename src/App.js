c719da85205fd328fdfb697357dda510b151f96f
dmoneyballer <drew.gilliesbc@gmail.com>
2019-03-11 12:13:12 -0600

    working prototype

--- src/App.js
+++ src/App.js
@@ -3,22 +3,28 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      count: 0
+    }
+    this.handleClick = () => {
+      this.setState({ count: this.state.count + 1})
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            This is the React Chrome Extension we've all been waiting for
+            A button counter
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <button onClick={this.handleClick}>Add to Count</button>
+          <h1>{this.state.count}</h1>
         </header>
       </div>
     );
